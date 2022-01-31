6fdaf94986633da896f1d27843a875cbaa4d0bc6
Vikrant1248 <97909263+Vikrant1248@users.noreply.github.com>
2022-01-31 10:23:33 +0530

    Add files via upload

---
+++ JS/Login.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    if(document.getElementById("username-field").value == "vikrant" && document.getElementById("password-field").value == "mandake")
+    {
+     window.open("./PortFolio.html")
+    }
+    else{
+        alert("invalid")
+    }
+ 
+ 
+ }
